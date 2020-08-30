35b798978877e3a14bd4eabe1788ee803b9bbd4b
Mingxen Chuah <mingxen.chuah@gmail.com>
2020-08-31 08:49:41 +1000

    Add sample values

--- src/vars/vars.sh
+++ src/vars/vars.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # AWS credentials
-AWS_ACCESS_KEY=""
-AWS_SECRET_KEY=""
-AWS_REGION=""
+AWS_ACCESS_KEY="some_value"
+AWS_SECRET_KEY="some_value"
+AWS_REGION="ap-southeast-2"
 
 # EC2 build config
-INSTANCE_TYPE=""
-KEY_PAIR_NAME=""
-VPC_ID=""
+INSTANCE_TYPE="t2.micro"
+KEY_PAIR_NAME="some_name"
+VPC_ID="vpc-123456"
 # Minimum 2 subnets across 2 AZ's required.
-SUBNET_ID_LIST='["",""]'
+SUBNET_ID_LIST='["subnet-123456","subnet-654321"]'
