d884aca292c37ef6bafcfa5b4d703538b68ce002
Mingxen Chuah <mingxen.chuah@gmail.com>
2020-08-30 20:04:42 +1000

    Added console output.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo -e '\n\n========== BUILD DOCKER IMAGE ==========\n\n'
+
 docker build . -t exercise/builder:latest
+
+echo -e '\n\n========== STARTING BUILD & DEPLOY ==========\n\n'
 
 docker run --rm -t \
     -v $(pwd)/src:/src exercise/builder:latest \
