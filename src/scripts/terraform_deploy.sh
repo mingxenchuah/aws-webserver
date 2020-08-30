7c37eb7dd4e59070e4c379604826df4136eacf9c
Mingxen Chuah <mingxen.chuah@gmail.com>
2020-08-30 15:21:12 +1000

    Added load balancing.

--- src/scripts/terraform_deploy.sh
+++ src/scripts/terraform_deploy.sh
@@ -11,7 +11,7 @@ aws_secret_key="$AWS_SECRET_KEY"
 image_id="$AMI_ID"
 instance_type="$INSTANCE_TYPE"
 vpc_id="$VPC_ID"
-subnet_id="$SUBNET_ID"
+subnet_id_list=$SUBNET_ID_LIST
 key_name="$KEY_PAIR_NAME"
 EOF
 
